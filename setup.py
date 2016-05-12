88ee68b885c2a28f3e09e5f93e7e239821f5d5e9
Fraser Harris <fraser.harris@gmail.com>
2016-05-12 13:10:33 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+  name = "threading_extensions",
+  version = "1.0",
+  description = "Useful threading.Thread Extensions",
+  packages = ["threading_extensions"],
+  author='Fraser Harris',
+  author_email='fraser.harris@gmail.com',
+  url='https://github.com/fraserharris/threading-extensions')
